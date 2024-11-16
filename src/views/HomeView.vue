27f2d9f6b54e021271024f5277df9e19eb4840fa
icz3us <167548188+icz3us@users.noreply.github.com>
2024-11-16 08:56:05 +0800

    Updated HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,85 +1,76 @@
 <template>
-  <div class="cards-section d-sm-flex" style="margin-top: 5px; margin-right: 500px;margin-left: 150px;">
-    <div class="row justify-content-center" >
-      <div class="col-md-6 col-lg-6 mb-4"> <!-- First card -->
-        <div class="dashboard-card">
-          <div class="card custom-card h-100">
-            <div class="card-body position-relative">
-              <div class="card-content">
-                <h5 class="card-title"><b>Students</b></h5>
-                <p class="card-text">????</p>
+  <div class="container-md position-absolute top-50 start-50 translate-middle ">
+    <div class="cards-section ">
+      <div class="row">
+        <!-- Card 1: Students -->
+        <div class="col-md-6 col-lg-6 mb-4 ">
+          <div class="dashboard-card">
+            <div class="card custom-card h-100">
+              <div class="card-body position-relative">
+                <div class="card-content">
+                  <h5 class="card-title"><b>Students</b></h5>
+                  <p class="card-text">????</p>
+                </div>
+                <img src="/public/images/Group.png" alt="Class Clearance" class="card-img">
               </div>
-              <img src="/public/images/Group.png" alt="Class Clearance" class="card-img">
             </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-md-6 col-lg-6 mb-4"> <!-- Second card -->
-        <div class="dashboard-card">
-          <div class="card custom-card h-100">
-            <div class="card-body position-relative">
-              <div class="card-content">
-                <h5 class="card-title"><b>Classes</b></h5>
-                <p class="card-text">????</p>
+        <!-- Card 2: Classes -->
+        <div class="col-md-6 col-lg-6 mb-4">
+          <div class="dashboard-card">
+            <div class="card custom-card h-100">
+              <div class="card-body position-relative">
+                <div class="card-content">
+                  <h5 class="card-title"><b>Classes</b></h5>
+                  <p class="card-text">????</p>
+                </div>
+                <img src="/public/images/Group2.png" alt="Take Attendance" class="card-img">
               </div>
-              <img src="/public/images/Group2.png" alt="Take Attendance" class="card-img">
             </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-md-6 col-lg-6 mb-4"> <!-- Third card -->
-        <div class="dashboard-card">
-          <div class="card custom-card h-100">
-            <div class="card-body position-relative">
-              <div class="card-content">
-                <h5 class="card-title"><b>Class Sections</b></h5>
-                <p class="card-text">????</p>
+        <!-- Card 3: Class Sections -->
+        <div class="col-md-6 col-lg-6 mb-4">
+          <div class="dashboard-card">
+            <div class="card custom-card h-100">
+              <div class="card-body position-relative">
+                <div class="card-content">
+                  <h5 class="card-title"><b>Class Sections</b></h5>
+                  <p class="card-text">????</p>
+                </div>
+                <img src="/public/images/Group3.png" alt="Personal Information" class="card-img">
               </div>
-              <img src="/public/images/Group3.png" alt="Personal Information" class="card-img">
             </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-md-6 col-lg-6 mb-4"> <!-- Fourth card -->
-        <div class="dashboard-card">
-          <div class="card custom-card h-100">
-            <div class="card-body position-relative">
-              <div class="card-content">
-                <h5 class="card-title"><b>Total of Attendance</b></h5>
-                <p class="card-text">????</p>
+        <!-- Card 4: Total of Attendance -->
+        <div class="col-md-6 col-lg-6 mb-4">
+          <div class="dashboard-card">
+            <div class="card custom-card h-100">
+              <div class="card-body position-relative">
+                <div class="card-content">
+                  <h5 class="card-title"><b>Total of Attendance</b></h5>
+                  <p class="card-text">????</p>
+                </div>
+                <img src="/public/images/Group4.png" alt="Create Class" class="card-img">
               </div>
-              <img src="/public/images/Group4.png" alt="Create Class" class="card-img">
             </div>
           </div>
         </div>
       </div>
     </div>
+    <!-- Bottom Blank Card -->
+    <div class="blank-card position-absolute top-100 start-50 translate-middle" style="margin-top: 50px;"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeView',
-  data() {
-    return {
-      showForm: false,
-      className: '',
-      classCapacity: ''
-    };
-  },
-  methods: {
-    submitForm() {
-      if (this.className && this.classCapacity) {
-        console.log('Form submitted with Class Name:', this.className, 'and Class Capacity:', this.classCapacity);
-        this.showForm = false;
-      } else {
-        alert('Please fill in both fields.');
-      }
-    }
-  }
 };
 </script>
 
@@ -98,7 +89,7 @@ export default {
   color: black;
   border-radius: 15px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-  width: 485px;
+  width: 100%; /* Ensure responsiveness */
   height: 240px;
   transition: background-color 0.3s ease;
 }
@@ -109,16 +100,16 @@ export default {
 }
 
 .card-content {
-  margin-top: 20px; 
+  margin-top: 20px;
 }
 
 .card-title {
-  font-size: 1.5rem; 
+  font-size: 1.5rem;
   margin-bottom: 0.5rem;
 }
 
 .card-text {
-  font-size: 1.2rem; 
+  font-size: 1.2rem;
 }
 
 .card-body {
@@ -129,9 +120,27 @@ export default {
 .card-img {
   width: 80px;
   height: 80px;
-  
   position: absolute;
-  bottom: 30px; 
-  right: 50px; 
+  bottom: 30px;
+  right: 50px;
+}
+
+.blank-card {
+  width: 1558px;
+  height: 70px;
+  background-color: #96D4DE;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+
+  margin:  auto;
+}
+
+@media (max-width: 768px) {
+  .card-img {
+    right: 30px;
+  }
+
+  .blank-card {
+    width: 100%;
+  }
 }
 </style>
